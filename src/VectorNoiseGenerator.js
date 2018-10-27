4304c7d7fd716dc315eb78ea91409acd1ea4a197
Jacek Pietal <jacek@gamecodehq.com>
2018-10-27 22:49:38 +0200

    custom random generator as option

--- src/VectorNoiseGenerator.js
+++ src/VectorNoiseGenerator.js
@@ -1,28 +1,39 @@
-const { randDir, lerp, floor, dot } = require("./util")
-const smooth = require("atlas-quintic-smoothing")
+'use strict'
+
+const { randDir, lerp, floor, dot } = require('./util')
+const smooth = require('atlas-quintic-smoothing')
 
 module.exports = class VectorNoiseGenerator {
-  constructor(width, height=width){
-    if (!width) throw new Error("grid width required");
+  constructor(width, height = width, rand = Math.random) {
+    if (!width) throw new Error('grid width required');
     const grid = [];
     this.grid = grid, this.width = width, this.height = height;
-    for (let i = width; i--;){
+    for (let i = width; i--;) {
       let r = (grid[i] = [])
-      for (let j = height; j--;){
-        r[j] = randDir();
+      for (let j = height; j--;) {
+        r[j] = randDir(rand);
       }
     }
   }
-  getPixel(x, y){
-    const { grid: g, width: w, height: h } = this;
-    x = x%w, y = y%h; // periodicity preferred over boundary errors
-    const xf = floor(x), yf = floor(y), xc = (xf+1)%w, yc = (yf+1)%h;
-    const dcy = y-(yc<y?h:yc), dcx = x-(xc<x?w:xc)
-    const sx = smooth(x-xf), sy = smooth(y-yf)
+  getPixel(x, y) {
+    const {
+      grid: g,
+      width: w,
+      height: h
+    } = this;
+    x = x % w, y = y % h; // periodicity preferred over boundary errors
+    const xf = floor(x),
+      yf = floor(y),
+      xc = (xf + 1) % w,
+      yc = (yf + 1) % h;
+    const dcy = y - (yc < y ? h : yc),
+      dcx = x - (xc < x ? w : xc)
+    const sx = smooth(x - xf),
+      sy = smooth(y - yf)
     return (lerp(
-      lerp(dot(g[xf][yf], [x-xf,y-yf]), dot(g[xf][yc], [x-xf,dcy]), sy),
-      lerp(dot(g[xc][yf], [dcx,y-yf]), dot(g[xc][yc], [dcx,dcy]), sy),
+      lerp(dot(g[xf][yf], [x - xf, y - yf]), dot(g[xf][yc], [x - xf, dcy]), sy),
+      lerp(dot(g[xc][yf], [dcx, y - yf]), dot(g[xc][yc], [dcx, dcy]), sy),
       sx
-    ) + 1)/2
+    ) + 1) / 2
   }
 }
--- src/util.js
+++ src/util.js
@@ -1,16 +1,16 @@
-const rand = () => Math.random()
+'use strict'
 
 const floor = n => Math.floor(n)
 
-const lerp = (a, b, x) => x*(b-a) + a;
+const lerp = (a, b, x) => x * (b - a) + a;
 
-const dot = (v1, v2) => v1[0]*v2[0]+v1[1]*v2[1]
+const dot = (v1, v2) => v1[0] * v2[0] + v1[1] * v2[1]
 
 const norm = v => {
-  const mag = Math.sqrt(dot(v,v));
-  return [v[0]/mag, v[1]/mag]  
+  const mag = Math.sqrt(dot(v, v));
+  return [v[0] / mag, v[1] / mag]
 }
 
-const randDir = () => norm([rand()-.5, rand()-.5])
+const randDir = (rand = Math.random) => norm([rand() - .5, rand() - .5])
 
-module.exports = { randDir, floor, lerp, dot } 
+module.exports = { randDir, floor, lerp, dot }
